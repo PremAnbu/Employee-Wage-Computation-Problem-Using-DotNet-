f2796712f058e1bc82fed25430ae498ca207b7b1
PremAnbu <premanbu9360@gmail.com>
2024-02-06 14:32:02 +0530

    Added full time and part time Employee Wage

--- EmployeeMain.cs
+++ EmployeeMain.cs
@@ -12,12 +12,23 @@ namespace EmployeeWage
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
             Random random = new Random();
-            int atten = random.Next(0,1);
+            int atten = random.Next(0,2);
             if (atten == 0)
                 Console.WriteLine("Employee Absent");
             else
                 Console.WriteLine("Employee Present");
-
+            CalculateEmployeeWage(20,8);
+            CalculatePartTimeEmployeeWage(20,8);
+        }
+        public static void CalculateEmployeeWage(int wagePerHour,int fullDayHour)
+        {
+            int totalWage=wagePerHour*fullDayHour;
+            Console.WriteLine("Total Wage for Full time Employee is {0}", totalWage);
+        }
+        public static void CalculatePartTimeEmployeeWage(int wagePerHour, int partTimeHour)
+        {
+            int totalWage = wagePerHour * partTimeHour;
+            Console.WriteLine("Total Wage For Part Time Employee is {0}", totalWage);
         }
     }
 }
