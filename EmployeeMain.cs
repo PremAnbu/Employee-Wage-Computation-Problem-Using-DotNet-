012e88e2d6cad6d2ad1a64c8337744a51a0f12f0
PremAnbu <premanbu9360@gmail.com>
2024-02-06 12:29:34 +0530

    created main branch

---
+++ EmployeeMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeMain
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
+        }
+    }
+}
