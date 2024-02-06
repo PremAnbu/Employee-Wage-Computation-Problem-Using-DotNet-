8b1ff5a98cd7a95a0bc400bb78f471383570e164
PremAnbu <premanbu9360@gmail.com>
2024-02-06 17:16:57 +0530

    Compute Employee wage for Multiple Companies

--- EmployeeMain.cs
+++ EmployeeMain.cs
@@ -8,27 +8,24 @@ namespace EmployeeWage
 {
     public class EmployeeMain
     {
-       const int partTime = 0;
-       const int fullTime = 1;
-       const int empRatePerHour = 20;
-       const int NoOfWorkingDays = 2;
-        const int maxHoursInMonth = 10;
-        public static void CalculateWage()
+       const int partTime = 1;
+       const int fullTime = 2;
+        public static int CalculateWage(string company,int empRatePerHour,int NoOfWorkingDays,int maxHoursInMonth)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch");
             int empHours = 0, totalEmpHour = 0, totalWorkingDays = 0;
-            while (totalEmpHour<=maxHoursInMonth && totalWorkingDays<NoOfWorkingDays)
+            while (totalEmpHour <= maxHoursInMonth && totalWorkingDays < NoOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
-                int atten = random.Next(0, 2);
+                int atten = random.Next(0, 3);
                 switch (atten)
                 {
                     case partTime:
-                        empHours = 40;
+                        empHours = 4;
                         break;
                     case fullTime:
-                        empHours = 80;
+                        empHours = 8;
                         break;
                     default:
                         empHours = 0;
@@ -37,11 +34,14 @@ namespace EmployeeWage
                 totalEmpHour =totalEmpHour + empHours;
                 Console.WriteLine("Employee Days : {0} Employee Hours {1}",totalWorkingDays,empHours );
             }
-            Console.WriteLine("Total Employee wage : {0}", totalEmpHour * empRatePerHour);
+            int totalEmployeeWage = totalEmpHour * empRatePerHour;
+            Console.WriteLine("Total Employee wage for company: {0} , is {1}",company,totalEmployeeWage);
+            return totalEmployeeWage;
         }
         public static void Main()
         {
-            CalculateWage();
+            CalculateWage("prem",20,2,10);
+            CalculateWage("Kumar", 10, 4, 20);
         }
     }
 }
